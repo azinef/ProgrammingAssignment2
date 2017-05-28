f2f3db5562a54ecb344730badf5d269f1e8c294c
azinef <ae5xc@mst.edu>
2017-05-27 20:55:36 -0500

    The final solution of R programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+## initialize empty matrices
+## creates the cache which is a list of functions, set, get, setinv, getinv
 makeCacheMatrix <- function(x = matrix()) {
   m<- NULL
   set<- function(y){
@@ -7,17 +9,21 @@ makeCacheMatrix <- function(x = matrix()) {
   get<-function()x
   setinverse<-function(inverse) m<<-inverse
   getinverse<-function()m
+  ##this list defines the output of the function of makecachematrix
   list(set=set,
        get=get,
        setinverse=setinverse,
        getinverse=getinverse)
 }
+## cachesolve returns inverse of the original matrix input to makeCacheMatrix()
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
+  ## if the inverse has been calculated, get it from the cache and skips the computation.
   if (!is.null(m)) {
     message("cached data")
     return(m)
   }
+  ## if not, calculates the inverse
   data <- x$get()
   m <- solve(data, ...)
   x$setinverse(m)
